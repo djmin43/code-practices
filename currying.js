bb98ba79188bcc93123054b646f54f02e8657d18
jay <dj.min43@gmail.com>
2021-08-01 17:12:49 +0900

    compose

--- currying.js
+++ currying.js
@@ -1,12 +1,34 @@
-function multiply(a, b) {
-  return a * b;
-};
+function multiply (a, b) {
+  return a * b
+}
 
-function multiplyX(x) {
+function multiplyX (x) {
   return function (a) {
-    return multiply(a, x);
-  };
-};
+    return multiply (a, x)
+  }
+}
 
-const multiplyTwo = multiplyX(2);
-const multiplyThree = multiplyX(3);
+function add (a, b) {
+  return a + b
+}
+
+function addX (x) {
+  return function (a) {
+    return add (a, x)
+  }
+}
+
+const multiplyTwo = multiplyX(2)
+const multiplyThree = multiplyX(3)
+const addFour = addX(4)
+
+const formula = [
+  multiplyThree,
+  multiplyThree,
+  addFour,
+].reduce(function(prevFunc, nextFunc) {
+  return function(x) {
+    // 연산
+    return nextFunc(prevFunc(x))
+  }
+}, k => k)
