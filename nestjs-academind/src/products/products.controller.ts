fef8f2b895f8065a3699f93c18188d1e6dd39698
jay <dj.min43@gmail.com>
2021-10-01 15:16:10 +0900

    new service

--- nestjs-academind/src/products/products.controller.ts
+++ nestjs-academind/src/products/products.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { ProductService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productSerivce: ProductService) {}
+
+  @Post()
+  addProduct(
+    @Body('title') prodTitle: string,
+    @Body('description') prodDesc: string,
+    @Body('price') prodPrice: number,
+  ): void {
+    this.productSerivce.insertProduct(prodTitle, prodDesc, prodPrice);
+  }
 }
