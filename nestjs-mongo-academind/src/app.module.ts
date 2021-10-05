53f1736158d7eae4c4ac461242402c24024eb77f
jay <dj.min43@gmail.com>
2021-10-05 10:27:37 +0900

    ceate mongodb data string

--- nestjs-mongo-academind/src/app.module.ts
+++ nestjs-mongo-academind/src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, 
+    ConfigModule.forRoot(), 
+    MongooseModule.forRoot(`mongodb+srv://jay:${process.env.DB_PASSWORD}@jaymin.jmuzg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)],
   controllers: [AppController],
   providers: [AppService],
 })
